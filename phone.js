e3f1f3da52d2575d2ad5f040397716cc1c07d3c0
unknown <mkaiser101@gmail.com>
2018-02-21 13:08:16 -0700

    added some blog stuff

--- phone.js
+++ phone.js
@@ -1,22 +1,11 @@
 $(document).ready(function() { //do on load document
-		$("#add_page").show(); //show element with ID "dial_page"
-		$("#contacts_page").hide(); //hide element with ID "contact_page"
-		$("#gesture_page").hide();
+		$("#content").show(); //show element with ID "dial_page"
 		$("#alerty").hide();
 
 });
 
 $("#button_add").click(function() { // when "dial_butt" is clicked
-	$("#add_page").show(); // show dial page
-	$("#alerty").show(); 
-	$("#contacts_page").hide();// hide contact page
-});
+	$("#content").show(); // show dial page
+	$("#alerty").show();
 
-$("#contact_butt").click(function() { // when "contact_butt" is clicked
-	$("#dial_page").hide(); // hide dial page
-	$("#contacts_page").show(); // show contact page
-	$("#add_page").hide();	// hide add page
-	$("#gesture_page").hide();
 });
-
-document.getElementById("button_add").className = "";
